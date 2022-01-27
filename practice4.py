fa0594e323ac13015b3928ba9cc367c525212ba1
JaeHyeong Ko <kojae2423@naver.com>
2022-01-27 21:28:04 +0900

    파이썬연습

---
+++ practice4.py
@@ -0,0 +1,15 @@
+##숫자처리함수##
+
+print(abs(-5)) #절댓값 5
+print(pow(4, 2)) # 4^2 = 16 거듭제곱
+print(max(5, 12)) # 12 최댓값
+print(min(5, 12)) # 5 최솟값
+print(round(3.14)) # 3 반올림
+print(round(4.999)) # 5
+
+from math import * # 나중에 배울것. 
+                   # math 라이브러리에 있는 모든것을 쓰겠다.
+ 
+print(floor(4.99)) # 내림. 4
+print(ceil(3.14)) # 올림. 4
+print(sqrt(16)) # 제곱근. 4
---
+++ practice5.py
@@ -0,0 +1,22 @@
+##랜덤함수## (난수)
+
+from random import * # random 라이브러리를 쓰겠다.
+
+print(random()) # 0.0 ~ 1.0 미만의 임의의 값 생성
+print(random() * 10) # 0.0 ~ 10.0 미만의 임의의 값 생성
+print(int(random() * 10)) # 0 ~ 10 미만의 임의의 값 생성
+                          # int는 정수 설정
+print(int(random() * 10))
+print(int(random() * 10))                        
+print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+
+print(int(random() * 45) + 1) # 1 ~ 45이하의 임의의 값 생성
+
+print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+
+print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
