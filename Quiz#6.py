e3509b998a05fd9ae94b3ff1b06185e4619496f8
JaeHyeong Ko <kojae2423@naver.com>
2022-02-10 23:25:30 +0900

    함수부분 퀴즈

--- Quiz#6.py
+++ Quiz#6.py
@@ -15,3 +15,14 @@
 # 키 175cm 남자의 표준 체중은 67.38kg 입니다.
 
 ##############정답###############
+
+def std_weight(height, gender) : # 키 m 단위 (실수), 성별 "남자" / "여자"
+    if gender == "남자" :
+        return height * height * 22
+    else : 
+        return height * height * 21
+
+height = 175 # cm 단위
+gender = "남자"
+weight = round(std_weight(height / 100, gender),2)
+print("키 {0}cm {1}의 표준 체중은 {2}kg 입니다.".format(height, gender, weight))
